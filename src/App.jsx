c6962f4983f9200f37fbb5cdb4e61971d64feaca
beqa <beqa.tskhvediani@gmail.com>
2024-02-27 21:25:38 +0400

    add data objects

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,28 @@ import "./reset.css";
 import "./App.css";
 
 function App() {
+  const basic = {
+    title: "Basic",
+    price: { monthly: "19.99", yearly: "199.99" },
+    storage: "500 GB Storage",
+    users: "2 Users Allowed",
+    send: "Send up to 3 GB",
+  };
+  const professional = {
+    title: "Professional",
+    price: { monthly: "24.99", yearly: "249.99" },
+    storage: "1 TB Storage",
+    users: "5 Users Allowed",
+    send: "Send up to 10 GB",
+  };
+  const master = {
+    title: "Master",
+    price: { monthly: "39.99", yearly: "239.99" },
+    storage: "2 TB Storage",
+    users: "10 Users Allowed",
+    send: "Send up to 20 GB",
+  };
+
   return (
     <>
       <h2>Our Pricing</h2>
