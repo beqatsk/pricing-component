686f0b7d6b87fea08e1481b53200c94ea596c8b3
beqa <beqa.tskhvediani@gmail.com>
2024-02-27 19:22:52 +0400

    add toggle-btn

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,14 @@ import "./App.css";
 function App() {
   return (
     <>
-      <div>hello</div>
+      <h2>Our Pricing</h2>
+      <div className="toggle-menu">
+        <p className="annually">Annually</p>
+        <div className="toggle-btn">
+          <div className="circle"></div>
+        </div>
+        <p className="monthly">Monthly</p>
+      </div>
     </>
   );
 }
