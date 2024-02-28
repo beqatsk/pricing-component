0213c0bb6d93f83ef649ef05fc74ac1fb8c835f1
beqa <beqa.tskhvediani@gmail.com>
2024-02-28 10:02:23 +0400

    create toggle component

--- src/App.jsx
+++ src/App.jsx
@@ -1,89 +1,45 @@
 import "./reset.css";
 import "./App.css";
 import date from "./data.json";
+import Toggle from "./components/toggle";
 
 function App() {
-  // const basic = {
-  //   title: "Basic",
-  //   price: { monthly: "19.99", yearly: "199.99" },
-  //   storage: "500 GB Storage",
-  //   users: "2 Users Allowed",
-  //   send: "Send up to 3 GB",
-  // };
-  // const professional = {
-  //   title: "Professional",
-  //   price: { monthly: "24.99", yearly: "249.99" },
-  //   storage: "1 TB Storage",
-  //   users: "5 Users Allowed",
-  //   send: "Send up to 10 GB",
-  // };
-  // const master = {
-  //   title: "Master",
-  //   price: { monthly: "39.99", yearly: "239.99" },
-  //   storage: "2 TB Storage",
-  //   users: "10 Users Allowed",
-  //   send: "Send up to 20 GB",
-  // };
-
   return (
     <>
-      <h2>Our Pricing</h2>
-      <div className="toggle-menu">
-        <p className="annually">Annually</p>
-        <div className="toggle-btn">
-          <div className="circle"></div>
-        </div>
-        <p className="monthly">Monthly</p>
-      </div>
-
+      <Toggle />
       <div className="list-container">
-        <div className="main-container">
-          <h3 className="title">{date[0].title}</h3>
-          <h1 className="price">
-            <span className="currency">$</span>
-            {date[0].price.monthly}
-          </h1>
-          <div className="line"></div>
-          <h4 className="detail">{date[0].storage}</h4>
-          <div className="line"></div>
-          <h4 className="detail">{date[0].users}</h4>
-          <div className="line"></div>
-          <h4 className="detail">{date[0].send}</h4>
-          <div className="line"></div>
-          <button className="learn-button">LEARN MORE</button>
-        </div>
-
-        <div className="main-container-active">
-          <h3 className="title-active">{date[1].title}</h3>
-          <h1 className="price-active">
-            <span className="currency-active">$</span>
-            {date[1].price.monthly}
-          </h1>
-          <div className="line-active"></div>
-          <h4 className="detail-active">{date[1].storage}</h4>
-          <div className="line-active"></div>
-          <h4 className="detail-active">{date[1].users}</h4>
-          <div className="line-active"></div>
-          <h4 className="detail-active">{date[1].send}</h4>
-          <div className="line-active"></div>
-          <button className="learn-button-active">LEARN MORE</button>
-        </div>
-
-        <div className="main-container">
-          <h3 className="title">{date[2].title}</h3>
-          <h1 className="price">
-            <span className="currency">$</span>
-            {date[2].price.monthly}
-          </h1>
-          <div className="line"></div>
-          <h4 className="detail">{date[2].storage}</h4>
-          <div className="line"></div>
-          <h4 className="detail">{date[2].users}</h4>
-          <div className="line"></div>
-          <h4 className="detail">{date[2].send}</h4>
-          <div className="line"></div>
-          <button className="learn-button">LEARN MORE</button>
-        </div>
+        {date.map((item, index) => (
+          <div
+            key={index}
+            className={`main-container${index === 1 ? "-active" : ""}`}
+          >
+            <h3 className={`title${index === 1 ? "-active" : ""}`}>
+              {item.title}
+            </h3>
+            <h1 className={`price${index === 1 ? "-active" : ""}`}>
+              <span className={`currency${index === 1 ? "-active" : ""}`}>
+                $
+              </span>
+              {item.price.monthly}
+            </h1>
+            <div className={`line${index === 1 ? "-active" : ""}`}></div>
+            <h4 className={`detail${index === 1 ? "-active" : ""}`}>
+              {item.storage}
+            </h4>
+            <div className={`line${index === 1 ? "-active" : ""}`}></div>
+            <h4 className={`detail${index === 1 ? "-active" : ""}`}>
+              {item.users}
+            </h4>
+            <div className={`line${index === 1 ? "-active" : ""}`}></div>
+            <h4 className={`detail${index === 1 ? "-active" : ""}`}>
+              {item.send}
+            </h4>
+            <div className={`line${index === 1 ? "-active" : ""}`}></div>
+            <button className={`learn-button${index === 1 ? "-active" : ""}`}>
+              LEARN MORE
+            </button>
+          </div>
+        ))}
       </div>
     </>
   );
---
+++ src/components/toggle.jsx
@@ -0,0 +1,16 @@
+// import React from "react";
+const Toggle = () => {
+  return (
+    <div>
+      <h2>Our Pricing</h2>
+      <div className="toggle-menu">
+        <p className="annually">Annually</p>
+        <div className="toggle-btn">
+          <div className="circle"></div>
+        </div>
+        <p className="monthly">Monthly</p>
+      </div>
+    </div>
+  );
+};
+export default Toggle;
